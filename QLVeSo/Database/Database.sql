24437ed1e60475c13419b15db3c712ffa33a90bc
HeoBayMau <hieu.trung.030197@gmail.com>
2018-10-03 18:45:05 +0700

    Update

--- QLVeSo/Database/Database.sql
+++ QLVeSo/Database/Database.sql
@@ -25,8 +25,8 @@ CREATE TABLE LoaiVeSo
 
 CREATE TABLE DangKy
 (
+	Id UNIQUEIDENTIFIER PRIMARY KEY,
 	IdDaiLy UNIQUEIDENTIFIER NOT NULL,
-	IdVeSo UNIQUEIDENTIFIER NOT NULL,
 	NgayDangKy DATE,
 	SoLuong INT
 )
@@ -44,9 +44,7 @@ CREATE TABLE PhanPhoi
 --CONSTRAINT--
 ALTER TABLE DangKy
 	ADD
-		CONSTRAINT FK_DangKy_LoaiVeSo_MaVeSo FOREIGN KEY (IdVeSo) REFERENCES LoaiVeSo(Id),
-		CONSTRAINT FK_DangKy_DaiLy_MaDaiLy FOREIGN KEY (IdDaiLy) REFERENCES DaiLy(Id),
-		CONSTRAINT PK_DangKy_MaDaiLy_MaVeSo PRIMARY KEY (IdDaiLy, IdVeSo)
+		CONSTRAINT FK_DangKy_DaiLy_MaDaiLy FOREIGN KEY (IdDaiLy) REFERENCES DaiLy(Id)
 GO
 
 ALTER TABLE PhanPhoi
@@ -70,3 +68,46 @@ INSERT INTO LoaiVeSo
 		  ('5cf29f46-bc5b-4b4f-b6a4-cfa74057764f', 'VSBH', N'Biên Hoà', N'Không khoá'),
 		  ('f7e693cc-2cc9-4141-b6bf-d90bb1d7bd63', 'VSPT', N'Phan Thiết', N'Không khoá'),
 		  ('aa0a7b91-85e6-4d14-ae3b-ad1a6be67d65', 'VSPR', N'Phan Rang', N'Không khoá')
+
+INSERT INTO DaiLy
+	VALUES('1d627550-fdcb-4df8-8141-a5ad9900fb2b','DL01',N'Đại lý 1',N'26 Nguuyễn Trãi',N'0935263748',N'Không khoá'),
+		  ('0064e9b1-2992-4258-b800-d4bafc43eaf7','DL02',N'Đại lý 2',N'73 An Dương Vương',N'0943728574',N'Không khoá'),
+		  ('44fa4066-84b9-44ec-924b-8ca535314cd3','DL03',N'Đại lý 3',N'94 Kinh Dương Vương',N'0912345673',N'Không khoá'),
+		  ('4411c19d-e4c2-46b6-8aa1-0fde5a36234d','DL04',N'Đại lý 4',N'122 Vĩnh Viễn',N'0962835263',N'Không khoá'),
+		  ('74cdbb04-a926-4221-83c3-9643b846199b','DL05',N'Đại lý 5',N'12 Hoà Hảo',N'0946372839',N'Không khoá')
+
+INSERT INTO DangKy
+	VALUES('397c5455-1fd5-4bb3-9743-7d04affaa522','1d627550-fdcb-4df8-8141-a5ad9900fb2b','2018-05-01',50),
+		  ('e1017914-e230-4926-a553-fff6ecfabbb7','1d627550-fdcb-4df8-8141-a5ad9900fb2b','2018-06-01',80),
+		  ('d361af43-9f6c-4c4b-9b5d-30419b902000','0064e9b1-2992-4258-b800-d4bafc43eaf7','2018-02-02',100),
+		  ('800ca87b-be3d-4225-abf2-0ba90f1c2d76','44fa4066-84b9-44ec-924b-8ca535314cd3','2018-03-06',150)
+
+--CREATE PROC ThemSoLuongGiao
+--AS
+--	--Khai báo con trỏ, lấy giá trị từng row--
+--	DECLARE CUR CURSOR FOR
+--		SELECT DISTINCT IdDaiLy,SoLuong
+--		FROM DangKy
+--		ORDER BY NgayDangKy DESC
+--	--
+--	INSERT INTO PhanPhoi
+--		VALUES(NEWID(), 
+--GO
+
+--select Table2.IdDaiLy, Table2.NgayDangKy, Table2.SoLuong
+--from
+--	(
+--	select  IdDaiLy as TempDaiLy, Max(NgayDangky) as TempNgay
+--	from DangKy 
+--	group by IdDaiLy 
+--	) as Table1, DangKy as Table2
+--where Table1.TempDaiLy = Table2.IdDaiLy and Table1.TempNgay=Table2.NgayDangKy
+
+
+--select  IdDaiLy as TempDaiLy, Max(NgayDangky) as TempNgay
+--	from DangKy 
+--	group by IdDaiLy 
+
+--	select  IdDaiLy as TempDaiLy, Max(NgayDangky) as TempNgay, sum(SoLuong)
+--	from DangKy 
+--	group by IdDaiLy,NgayDangKy
--- QLVeSo/ProjectQLVeSo/ProjectQLVeSo/Controllers/ManagementController.cs
+++ QLVeSo/ProjectQLVeSo/ProjectQLVeSo/Controllers/ManagementController.cs
@@ -18,52 +18,53 @@ namespace ProjectQLVeSo.Controllers
         {
             return View();
         }
-        
-        public IActionResult LoaiVeSo()
+
+        public IActionResult LoaiVeSo(string thongbao)
         {
+            if (thongbao != null)
+                ViewBag.ThongBao = thongbao;
             List<LoaiVeSo> dsLoaiVeSo = new List<LoaiVeSo>();
             dsLoaiVeSo = context.LoaiVeSo.OrderBy(vs => vs.Id).ToList();
             return View(dsLoaiVeSo);
         }
 
-        public IActionResult Search(string keyword)
+        public IActionResult Search(string txtSearch)
         {
             List<LoaiVeSo> dsLoaiVeSo = new List<LoaiVeSo>();
-            if (keyword == null)
-            {
-                dsLoaiVeSo = context.LoaiVeSo.ToList();
-            }
-            else
-            {
-                dsLoaiVeSo = context.LoaiVeSo.Where(vs => vs.Tinh.Contains(keyword)).ToList();
-            }
-            return PartialView("LoaiVeSoPartial", dsLoaiVeSo);
+            dsLoaiVeSo = context.LoaiVeSo.Where(vs => vs.Tinh.Contains(txtSearch)).ToList();
+            return View("LoaiVeSo", dsLoaiVeSo);
         }
 
         public IActionResult Add(string ma, string tinh)
         {
             try
             {
+                //Thông báo
+                string thongbao = "";
                 //Kiểm tra mã có trùng chưa
-                LoaiVeSo vs = new LoaiVeSo();
-                vs.Id = Guid.NewGuid();
-                vs.Ma = ma;
-                vs.Tinh = tinh;
-                vs.TinhTrang = "Không khoá";
-                context.LoaiVeSo.Add(vs);
-                context.SaveChanges();
-                return RedirectToAction("LoaiVeSo");
+                LoaiVeSo vs;
+                vs = context.LoaiVeSo.Where(v => v.Ma == ma).FirstOrDefault();
+                if (vs == null)
+                {
+                    vs = new LoaiVeSo();
+                    vs.Id = Guid.NewGuid();
+                    vs.Ma = ma;
+                    vs.Tinh = tinh;
+                    vs.TinhTrang = "Không khoá";
+                    context.LoaiVeSo.Add(vs);
+                    context.SaveChanges();
+                    thongbao = "Thêm thành công";
+                }
+                else
+                {
+                    thongbao = "Mã bị trùng";
+                }
+                return RedirectToAction("LoaiVeSo", "Management", new { thongbao = thongbao });
             }
             catch (Exception)
             {
-
                 throw;
             }
-        }
-
-        public IActionResult Test()
-        {
-            return View();
         }
     }
 }
