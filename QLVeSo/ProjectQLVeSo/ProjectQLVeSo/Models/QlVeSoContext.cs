317992636d67508ade6fdaa8cdce919ada1cbff8
Thanhnvsgu <32585608+Thanhnvsgu@users.noreply.github.com>
2018-10-10 19:00:25 +0700

    file_1

--- QLVeSo/ProjectQLVeSo/ProjectQLVeSo/Models/QlVeSoContext.cs
+++ QLVeSo/ProjectQLVeSo/ProjectQLVeSo/Models/QlVeSoContext.cs
@@ -29,7 +29,7 @@ namespace ProjectQLVeSo.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=HEoBAYMAU;Database=QLVeSo;Integrated Security=True;");
+                optionsBuilder.UseSqlServer("Server=DESKTOP-MEMEU1K\\THANHETN;Database=QLVeSo;Integrated Security=True;");
             }
         }
 
