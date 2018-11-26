6154319b6e2fd59e4932226cd06fd5b03ba67e60
HeoBayMau <hieu.trung.030197@gmail.com>
2018-11-26 13:00:21 +0700

    Update

---
+++ QLCuocDT/ProjectQLCuocDT/Services/BUS/LoaiCuocBUS.cs
@@ -0,0 +1,15 @@
+﻿using Services.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.BUS
+{
+    public class LoaiCuocBUS
+    {
+        private QLCuocDTEntities db = new QLCuocDTEntities();
+        
+    }
+}
