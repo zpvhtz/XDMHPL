eb4fb6ec9d3fdf24840db0beb4a9d7a9e8afa69a
HeoBayMau <hieu.trung.030197@gmail.com>
2018-11-30 09:44:17 +0700

    ...

--- QLCuocDT/ProjectQLCuocDT/WebForm/App_Code/QLCuocDT.Designer.cs
+++ QLCuocDT/ProjectQLCuocDT/WebForm/App_Code/QLCuocDT.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'F:\ASP.NET\XDMHPL\QLCuocDT\ProjectQLCuocDT\WebForm\App_Code\QLCuocDT.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\ASP.NET\XDMHPL\QLCuocDT\ProjectQLCuocDT\WebForm\App_Code\QLCuocDT.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
